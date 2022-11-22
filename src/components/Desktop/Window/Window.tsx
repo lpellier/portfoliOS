cf13ff38678b76338c76e759d2428767337fdf68
Lucas PELLIER <lpellier@e1r11p4.clusters.42paris.fr>
2022-11-22 18:35:13 +0100

    tried to implement p5 to react

--- src/components/Desktop/Window/Window.tsx
+++ src/components/Desktop/Window/Window.tsx
@@ -1,17 +1,10 @@
 import React from 'react';
-import ReactMarkdown from 'react-markdown';
-import remarkGfm from "remark-gfm";
-import remarkRehype from "remark-rehype";
-import { Scrollbar } from 'react-scrollbars-custom';
+// import { Scrollbar } from 'react-scrollbars-custom';
 import README from "./README.md"
 import './Window.css';
-
+import Comp from "./TestP5/TestP5"
 
 // ! https://www.copycat.dev/blog/react-markdown/
-
-// TODO 
-// ? Quit animation
-// ? Minimize
 
 class Window extends React.Component<{getLastZIndex: any, win_name : string, z_index: number, destroyWindow: any}> {
 	state = {
@@ -30,6 +23,8 @@ class Window extends React.Component<{getLastZIndex: any, win_name : string, z_i
 		dragging: false,
 		maximized: false,
 		maximized_once: false,
+		minimized: false,
+		quit: false,
 		width: 700,
 		height: 500,
 		oldWinX: window.innerWidth,
@@ -59,7 +54,7 @@ class Window extends React.Component<{getLastZIndex: any, win_name : string, z_i
 					x: 35 * this.props.z_index,
 				}
 			})
-		}
+		}else 
 		if (this.state.pos.y + (rect?rect.height:0) > window.innerHeight) {
 			this.setState({
 				pos: {
@@ -80,7 +75,7 @@ class Window extends React.Component<{getLastZIndex: any, win_name : string, z_i
 	}
 
 	resetZIndex(new_index: number): void {
-		this.setState({z_index: new_index})
+		this.setState({z_index: new_index, minimized: false})
 	}
 
 	handleMouseDown() {
@@ -134,12 +129,17 @@ class Window extends React.Component<{getLastZIndex: any, win_name : string, z_i
 		
 		render() {
 			let window_classes = "WindowDefault";
-			if (!this.state.maximized_once)
+			if (this.state.quit)
+				window_classes += " WindowQuit";
+			else if (this.state.minimized)
+				window_classes += " WindowMinimized";
+			else if (!this.state.maximized_once)
 				window_classes += " WindowSpawn";
 			else if (this.state.maximized)
 				window_classes += " WindowMaximized";
 			else if (!this.state.maximized)
 				window_classes += " WindowMaximizedReverse";
+			
 			return (
 			<div onMouseLeave={this.handleMouseUp.bind(this)} 
 				onMouseDown={this.handleMouseDown.bind(this)} 
@@ -150,18 +150,25 @@ class Window extends React.Component<{getLastZIndex: any, win_name : string, z_i
 				className={window_classes}>
 				<h3 className="WindowTitle">{this.props.win_name}</h3>
 				<div className='ButtonFlex'>
-					<button className="WindowButton" id='MinimizeButton'/>
+					<button className="WindowButton" id='MinimizeButton' onClick={() => {
+						this.setState({minimized: true})
+					}}/>
 					<button className="WindowButton" id='MaximizeButton' onClick={() => {
 						this.setState({maximized_once: true, maximized: !this.state.maximized}); 
 						this.props.getLastZIndex(this.props.win_name);}
 					}/>
-					<button className="WindowButton" id='QuitButton' onClick={() => this.props.destroyWindow(this.props.win_name)}/>
+					<button className="WindowButton" id='QuitButton' onClick={() => {
+						this.setState({quit: true})
+						setTimeout(() => {
+							this.props.destroyWindow(this.props.win_name)
+						}, 200)
+					}}/>
 				</div>
-				<div className='content'>
-					<Scrollbar className='Scrollbar' style={{ width: this.state.width * 0.9, height: this.state.height * 0.8 }}>
-						<ReactMarkdown remarkPlugins={[remarkGfm]} children={this.state.info}/>
-					</Scrollbar>
-				</div>
+				{/* <div className='content'> */}
+					{/* <Scrollbar className='Scrollbar' style={{ width: this.state.width * 0.9, height: this.state.height * 0.8 }}> */}
+				<Comp width={this.state.width * 0.9} height={this.state.height * 0.8}/>
+					{/* </Scrollbar> */}
+				{/* </div> */}
 			</div>
 		)
 	}
